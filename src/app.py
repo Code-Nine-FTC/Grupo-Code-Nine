3aba0841eb9370c7c0b1d11a234fd647b17d10ae
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2023-09-29 16:56:43 -0300

    Imagens da navbar inseridas
    Diretórios CSS e imagens da navbar inseridas

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+    
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
