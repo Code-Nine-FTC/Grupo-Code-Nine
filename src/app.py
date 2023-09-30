43b422073c566d2ce52a935ab5cfe4fc520fb598
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2023-09-30 15:16:46 -0300

    Arquivos bases criados
    Arquivos base de todas as telas criados

--- src/app.py
+++ src/app.py
@@ -15,5 +15,25 @@ def login():
 def cadastro():
     return render_template('cadastro.html')
 
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('/localizacao')
+def localizacao():
+    return render_template('localizacao.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
