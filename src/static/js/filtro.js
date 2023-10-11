bc2cf28df4ff4298bc4742d866465f0351f9e7ea
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2023-10-11 09:55:35 -0300

    Filtro atualizado
    Filtro automatizado com uso de for para gerar lista de estados e suas ids por meio de arrays

--- src/static/js/filtro.js
+++ src/static/js/filtro.js
@@ -1,29 +1,60 @@
 function myFunction() {
     var y = document.getElementById("regiao").value;
     var option = document.createElement("option");
-        
+    var select = document.getElementById("estado");
     if (y === ''){
         document.getElementById("estado").innerHTML=option.remove;
     }
 
     if (y === 'norte') {
-        document.getElementById("estado").innerHTML="<option value=amazonas>Amazonas</option><option>Pará</option>";
+        var est = new Array("Amazonas", "Pará", "Amapá", "Roraima", "Tocantins", "Rondônia", "Acre");
+        var estval = new Array("amazonas", "para", "amapa", "roraima", "tocantins", "rondonia", "acre");
+        document.getElementById("estado").innerHTML=option.remove;
+        for(var i = 0; i < est.length; i++) {
+            select[select.length] = new Option(est[i], estval[i])
+        }
+        
+        // document.getElementById("estado").innerHTML="<option value=amazonas>Amazonas</option><option value=para>Pará</option>";
     }
 
     if (y === 'nordeste') {
-        document.getElementById("estado").innerHTML="<option>Bahia</option><option>Salvador</option>";
+        // document.getElementById("estado").innerHTML="<option>Bahia</option><option>Salvador</option>";
+        var est = new Array("Pernambuco", "Maranhão", "Bahia", "Ceará", "Piauí", "Rio Grande do Norte", "Paraíba", "Sergipe", "Alagoas");
+        var estval = new Array("pernambuco", "maranhão", "bahia", "ceará", "piauí", "riograndedonorte", "paraíba", "sergipe", "alagoas");
+        document.getElementById("estado").innerHTML=option.remove;
+        for(var i = 0; i < est.length; i++) {
+            select[select.length] = new Option(est[i], estval[i])
+        }
     }
 
     if (y === 'centrooeste') {
-        document.getElementById("estado").innerHTML="<option>Brasília</option><option>Goías</option>";
+        // document.getElementById("estado").innerHTML="<option>Brasília</option><option>Goías</option>";
+        var est = new Array("Brasília", "Mato Grosso", "Mato Grosso do Sul", "Goiás");
+        var estval = new Array("brasilia", "matogrosso", "matogrossodosul", "goias");
+        document.getElementById("estado").innerHTML=option.remove;
+        for(var i = 0; i < est.length; i++) {
+            select[select.length] = new Option(est[i], estval[i])
+        }
     }
 
     if (y === 'sudeste') {
-        document.getElementById("estado").innerHTML="<option>São Paulo</option><option>Rio de Janeiro</option>";
+        // document.getElementById("estado").innerHTML="<option>São Paulo</option><option>Rio de Janeiro</option>";
+        var est = new Array("São Paulo", "Rio de Janeiro", "Espírito Santo", "Minas Gerais");
+        var estval = new Array("saopaulo", "riodejaneiro", "espiritosanto", "minasgerais");
+        document.getElementById("estado").innerHTML=option.remove;
+        for(var i = 0; i < est.length; i++) {
+            select[select.length] = new Option(est[i], estval[i])
+        }
     }
 
     if (y === 'sul') {
-        document.getElementById("estado").innerHTML="<option>Santa Catarina</option><option>Paraná</option>";
+        // document.getElementById("estado").innerHTML="<option>Santa Catarina</option><option>Paraná</option>";
+        var est = new Array("Rio Grande do Sul", "Santa Catarina", "Paraná");
+        var estval = new Array("riograndedosul", "santacatarina", "parana");
+        document.getElementById("estado").innerHTML=option.remove;
+        for(var i = 0; i < est.length; i++) {
+            select[select.length] = new Option(est[i], estval[i])
+        }
     }
 }
 
