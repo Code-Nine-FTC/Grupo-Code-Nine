640765d499b5a7826d1ecbc7dee033ed3677800e
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2023-10-26 11:58:13 -0300

    Sistema de logout criado
    Sistema de logout, permissão de acesso a tela de perfil e exibição de dados do usuário na tela perfil
    
    Co-Authored-By: Renato Cruz Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 app.secret_key = "codenine"  # Change this to a strong, random key
 
 #conecta ao banco de dados
-db = mysql.connector.connect(
-    host="localhost", #host = ip, no caso localhost
-    user="root", #usuario para logar
-    password="fatec", #senha
-    database="cianp" #qual banco de dados será utilizado
-)
+db = {
+    'host': "localhost", #host = ip, no caso localhost
+    'user': "root", #usuario para logar
+    'password': "fatec", #senha
+    'database': "cianp", #qual banco de dados será utilizado
+}
 
-cursor = db.cursor(buffered=True) #ativa a biblioteca cursor para acessar o banco de dados
+db_config = mysql.connector.connect(**db)
+
+cursor = db_config.cursor(buffered=True) #ativa a biblioteca cursor para acessar o banco de dados
 
 @app.route('/')
 def home():
@@ -26,17 +28,15 @@ def login():
         senha = request.form['senha']
 
         # Check if the username and password match
-        cursor.execute("SELECT * FROM usuario WHERE username = %s AND senha = %s", (email, senha)) 
+        cursor.execute("SELECT * FROM usuario WHERE email = %s AND senha = %s", (email, senha)) 
         user = cursor.fetchone() #verifica se os valores das variaveis username e password coincidem com os valores salvos no banco de dados
         #e insere na variavel user o valor True se os dados coincidirem, caso contrário insere False
 
         if user: #caso a variável user seja verdadeira
-            flash("Logado com sucesso") #exibe o alerta de login concluído
-            session['logged_in'] = True #define o status da sessão como True
-            return redirect(url_for('/')) #retorna o usuário para a tela home
+            session['user_email']=email
+            return redirect('/perfil')
         else:
-            flash("Dados errados, tente novamente") #caso a variável user seja falsa
-            #exibe um alerta de que os dados estão errados
+            return 'Inválido.'
     return render_template('login.html')
 
 @app.route('/cadastro', methods=['GET', 'POST']) #inicia a rota de get e post de dados no form html
@@ -51,14 +51,12 @@ def cadastro():
         parentesco = request.form['parentesco'] #adiciona o valor parentesco do form para a variavel parentesco
 
         try:
-            cursor.execute("INSERT INTO usuario (username, senha, email, cpf, prof, data_nasc, parentesco) VALUES (%s, %s, %s, %s, %s, %s, %s)", (username, senha, email, cpf, prof, data_nasc, parentesco))
-            db.commit() #insere os valores das variaveis username e password para suas respectivas colunas na tabela users
-            flash("Cadastro realizado, agora você pode logar.") #exibe um alerta de que a tarefa foi concluida
-            return redirect(url_for('login')) #retorna o usuário para a tela de login
-        except mysql.connector.Error as err:
-            flash(f"Error: {err}") #caso um erro ocorra, ele é armazenado na variavel err, e um alerta é exibido na tela informando o erro
-            db.rollback() #cancela a conexão ao banco de dados
-
+            cursor.execute("INSERT INTO usuario (username, email, cpf, prof, data_nasc, parentesco, senha) VALUES (%s, %s, %s, %s, %s, %s, %s)", (username, email, cpf, prof, data_nasc, parentesco, senha))
+            db_config.commit() #insere os valores das variaveis username e password para suas respectivas colunas na tabela users
+            alert("Cadastro realizado, agora você pode logar.") #exibe um alerta de que a tarefa foi concluida
+            return redirect(url_for('/login')) #retorna o usuário para a tela de login
+        except mysql.connector.errors.IntegrityError:
+            return 'Email já está em uso.'
     return render_template('cadastro.html')
 
 @app.route('/info')
@@ -81,12 +79,19 @@ def forum():
 def faq():
     return render_template('faq.html')
 
-@app.route('/perfil')
+@app.route('/perfil', methods = ['POST', 'GET'])
 def perfil():
-    if 'logged_in' in session and session['logged_in']: #caso a variável dde sessão seja definida como logada
-        return render_template('perfil.html')
-    else:
-        return redirect(url_for('login')) #caso contrário, redireciona o usuário para a tela de login
-
+    if 'user_email' in session:
+        email = session['user_email']
+        cursor.execute("SELECT * FROM usuario WHERE email = %s", (email,))
+        user = cursor.fetchone()
+        return render_template('perfil.html', user=user)
+    return redirect('/login')
+    
+@app.route('/logout', methods = ['POST'])
+def logout():
+    session.pop('user_email', None)
+    return redirect('/login')
+    
 if __name__ == "__main__":
     app.run(debug=True)
